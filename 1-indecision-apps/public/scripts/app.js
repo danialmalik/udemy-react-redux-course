999d807f5b5782a97ddbe3000cafc0a4f8a019dd
danialmalik <danialmalik321@gmail.com>
2020-08-03 22:04:07 +0500

    Initial Commit

---
+++ 1-indecision-apps/public/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// var template = <p>Hello</p>
+var template = /*#__PURE__*/React.createElement("p", null, "Hello");
+
+var appRoot = document.getElementById('root');
+ReactDOM.render(template, appRoot);
