2af2ccc03b8826d0de0d5dbeb61082c9077465ca
danialmalik <danialmalik321@gmail.com>
2020-08-03 22:43:07 +0500

    Base template

--- 1-indecision-apps/public/scripts/app.js
+++ 1-indecision-apps/public/scripts/app.js
@@ -1,7 +1,46 @@
 'use strict';
 
-// var template = <p>Hello</p>
-var template = /*#__PURE__*/React.createElement("p", null, "Hello");
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Make decision making easy',
+    options: ['One', 'Two']
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'title',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'Out of options'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        app.options.map(function (option, index) {
+            return React.createElement(
+                'li',
+                { key: index },
+                option
+            );
+        })
+    )
+);
 
 var appRoot = document.getElementById('root');
 ReactDOM.render(template, appRoot);
---
+++ 1-indecision-apps/src/app.js
@@ -0,0 +1,23 @@
+'use strict';
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Make decision making easy',
+    options: ['One', 'Two']
+}
+
+var template = (
+    <div>
+        <title>{app.title}</title>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'Out of options'}</p>
+        <ul>
+            {app.options.map((option, index) => (
+                <li key={index}>{option}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+var appRoot = document.getElementById('root');
+ReactDOM.render(template, appRoot);
