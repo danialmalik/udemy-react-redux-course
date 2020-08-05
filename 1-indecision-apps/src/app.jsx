238cdfa51c7d42108e59e4ce3a80bdffab47174c
danialmalik <danialmalik321@gmail.com>
2020-08-05 09:45:57 +0500

    upto events

--- 1-indecision-apps/src/app.jsx
+++ 1-indecision-apps/src/app.jsx
@@ -5,19 +5,49 @@ const app = {
     options: ['One', 'Two']
 }
 
-const template = (
-    <div>
-        <title>{app.title}</title>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'Out of options'}</p>
-        <ul>
-            {app.options.map((option, index) => (
-                <li key={index}>{option}</li>
-            ))}
-        </ul>
-    </div>
-);
+let count = 0;
 
 const appRoot = document.getElementById('root');
-ReactDOM.render(template, appRoot);
+
+const renderApp = () => {
+    console.log('rendering')
+    let template = (
+        <div>
+            <title>{app.title}</title>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'Out of options'}</p>
+            <ul>
+                {app.options.map((option, index) => (
+                    <li key={index}>{option}</li>
+                ))}
+            </ul>
+            <hr />
+            <h1>Count: {count}</h1>
+
+            <button onClick={() => {
+                console.log(++count);
+                renderApp();
+            }}>
+                +1
+            </button>
+
+            <button onClick={() => {
+                console.log(--count);
+                renderApp();
+            }}>
+                -1
+            </button>
+
+            <button onClick={() => {
+                console.log(count = 0);
+                renderApp();
+            }}>
+                reset
+            </button>
+
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
+renderApp();
