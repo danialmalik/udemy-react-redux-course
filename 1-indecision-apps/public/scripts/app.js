39f490203655e80cffbfddabd04f37898472a541
danialmalik <danialmalik321@gmail.com>
2020-08-20 11:50:32 +0500

    WIP: before webpack section

--- 1-indecision-apps/public/scripts/app.js
+++ 1-indecision-apps/public/scripts/app.js
@@ -1,99 +1,82 @@
 'use strict';
 
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Make decision making easy',
-    options: ['One', 'Two']
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var _react = require('react');
+
+var IndecisionApp = function IndecisionApp() {
+    var _useState = useState({
+        option: [1, 2]
+    }),
+        _useState2 = _slicedToArray(_useState, 2),
+        state = _useState2[0],
+        setState = _useState2[1];
+
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(Header, null),
+        React.createElement(Action, null),
+        React.createElement(Options, null)
+    );
 };
 
-var count = 0;
-
-var appRoot = document.getElementById('root');
-
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-
-    var option = e.target.elements.option.value;
-    option && app.options.push(option);
-
-    console.log('Added option', option);
-    renderApp();
-};
-
-var onRemoveAll = function onRemoveAll() {
-    app.options = [];
-    renderApp();
-};
-
-var onMakeDecision = function onMakeDecision() {
-    var random = Math.floor(Math.random() * app.options.length);
-    console.log(app.options[random]);
-    alert(app.options[random]);
-};
-
-var renderApp = function renderApp() {
-
-    var template = React.createElement(
+var Header = function Header(props) {
+    return React.createElement(
         'div',
         null,
         React.createElement(
-            'title',
-            null,
-            app.title
-        ),
-        React.createElement(
             'h1',
             null,
-            app.title
-        ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
+            'Indecision'
         ),
         React.createElement(
-            'p',
+            'h2',
             null,
-            app.options.length > 0 ? 'Here are your options' : 'Out of options'
-        ),
+            'Make decision making easy'
+        )
+    );
+};
+
+var Action = function Action(_ref) {
+    var options = _ref.options;
+
+    var handleOnClick = function handleOnClick(e) {
+        e.preventDefault();
+
+        var option = Math.floor(Math.random() * options.length);
+        alert(options[option]);
+    };
+
+    return React.createElement(
+        'div',
+        null,
         React.createElement(
-            'h3',
-            null,
-            app.options.length
-        ),
+            'button',
+            { onClick: handleOnClick },
+            'What should I do?'
+        )
+    );
+};
+
+var Options = function Options(_ref2) {
+    var options = _ref2.options;
+    return React.createElement(
+        'div',
+        null,
         React.createElement(
             'ul',
             null,
-            app.options.map(function (option, index) {
+            options.map(function (option, index) {
                 return React.createElement(
-                    'li',
+                    'option',
                     { key: index },
                     option
                 );
             })
-        ),
-        React.createElement(
-            'button',
-            { disabled: !app.options.length, onClick: onRemoveAll },
-            'Remove All'
-        ),
-        React.createElement(
-            'button',
-            { disabled: !app.options.length, onClick: onMakeDecision },
-            'What should I do?'
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option', placeholder: 'Enter option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add Option'
-            )
         )
     );
-    ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+var appRoot = document.getElementById('root');
+ReactDOM.render(React.createElement(IndecisionApp, null), appRoot);
--- 1-indecision-apps/src/app.jsx
+++ 1-indecision-apps/src/app.jsx
@@ -1,60 +1,61 @@
 'use strict';
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Make decision making easy',
-    options: ['One', 'Two']
-}
+import {userState} from 'react';
 
-let count = 0;
+const IndecisionApp = () => {
+    const [state, setState] = useState({
+        option: [
+            1,
+            2
+        ]
+    })
 
-const appRoot = document.getElementById('root');
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-    option && app.options.push(option);
-
-    console.log('Added option', option);
-    renderApp()
-}
-
-const onRemoveAll = () => {
-    app.options = [];
-    renderApp();
-}
-
-const onMakeDecision = () => {
-    const random = Math.floor(Math.random() * app.options.length);
-    console.log(app.options[random])
-    alert(app.options[random])
-}
-
-const renderApp = () => {
-
-    let template = (
+    return (
         <div>
-            <title>{app.title}</title>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'Out of options'}</p>
-            <h3>{app.options.length}</h3>
-            <ul>
-                {app.options.map((option, index) => (
-                    <li key={index}>{option}</li>
-                ))}
-            </ul>
-
-            <button disabled={!app.options.length} onClick={onRemoveAll}>Remove All</button>
-            <button disabled={!app.options.length} onClick={onMakeDecision}>What should I do?</button>
-            <form onSubmit={onFormSubmit}>
-                <input type='text' name='option' placeholder='Enter option'></input>
-                <button>Add Option</button>
-            </form>
+            <Header />
+            <Action />
+            <Options />
 
         </div>
     );
-    ReactDOM.render(template, appRoot);
 }
 
-renderApp()
+const Header = (props) => (
+    <div>
+        <h1>Indecision</h1>
+        <h2>
+            Make decision making easy
+        </h2>
+    </div>
+)
+
+
+const Action = ({ options }) => {
+    const handleOnClick = (e) => {
+        e.preventDefault();
+
+        const option = Math.floor(Math.random()*options.length);
+        alert(options[option])
+    }
+
+    return (
+        <div>
+            <button onClick={handleOnClick}>
+                What should I do?
+            </button>
+        </div>
+    )
+}
+
+const Options = ({ options }) => (
+    <div>
+        <ul>
+            {options.map((option, index) => (
+                <option key={index}>{option}</option>
+            ))}
+        </ul>
+    </div>
+)
+
+
+const appRoot = document.getElementById('root');
+ReactDOM.render(<IndecisionApp />, appRoot);
