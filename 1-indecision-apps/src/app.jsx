5c3f0758705f5acf021c79be15459c232bdf04a0
danialmalik <danialmalik321@gmail.com>
2020-09-29 21:22:22 +0500

    Make things working

--- 1-indecision-apps/src/app.jsx
+++ 1-indecision-apps/src/app.jsx
@@ -1,61 +1,52 @@
 'use strict';
-import {userState} from 'react';
+
+const options = [
+  'option 1',
+  'option 2',
+  'option 3',
+]
 
 const IndecisionApp = () => {
-    const [state, setState] = useState({
-        option: [
-            1,
-            2
-        ]
-    })
-
-    return (
-        <div>
-            <Header />
-            <Action />
-            <Options />
-
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header />
+      <Action options={options}/>
+      <Options options={options} />
+    </div>
+  );
+};
 
 const Header = (props) => (
-    <div>
-        <h1>Indecision</h1>
-        <h2>
-            Make decision making easy
-        </h2>
-    </div>
-)
-
+  <div>
+    <h1>Indecision</h1>
+    <h2>Make decision making easy</h2>
+  </div>
+);
 
 const Action = ({ options }) => {
-    const handleOnClick = (e) => {
-        e.preventDefault();
+  const handleOnClick = (e) => {
+    e.preventDefault();
 
-        const option = Math.floor(Math.random()*options.length);
-        alert(options[option])
-    }
+    const option = Math.floor(Math.random() * options.length);
+    alert(options[option]);
+  };
 
-    return (
-        <div>
-            <button onClick={handleOnClick}>
-                What should I do?
-            </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={handleOnClick}>What should I do?</button>
+    </div>
+  );
+};
 
 const Options = ({ options }) => (
-    <div>
-        <ul>
-            {options.map((option, index) => (
-                <option key={index}>{option}</option>
-            ))}
-        </ul>
-    </div>
-)
-
+  <div>
+    <ul>
+      {options.map((option, index) => (
+        <option key={index}>{option}</option>
+      ))}
+    </ul>
+  </div>
+);
 
 const appRoot = document.getElementById('root');
 ReactDOM.render(<IndecisionApp />, appRoot);
